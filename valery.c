658a1af3dcca1d505d1ed18fa25c50465ffba546
vintalius <vintalius@gmail.com>
2019-09-26 17:47:23 +0300

    secund commit

---
+++ valery.c
@@ -0,0 +1,42 @@
+/*
+ * valery.c
+ * 
+ * Copyright 2019 rtg <rtg@rtg-desktop>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+<<<<<<< bcc40af607cb4adfe3786205938ea1a0aa04c30e
+	printf("hello Valery sdfgjlskf\n");
+=======
+//<<<<<<< bcc40af607cb4adfe3786205938ea1a0aa04c30e
+//	printf("hello Valery sdfgjlskf\n");
+//=======
+	printf("hello Valery sdf\n");
+//>>>>>>> first commit
+	int a = 3/0;
+>>>>>>> first commit
+	
+	return -2222;
+}
+
